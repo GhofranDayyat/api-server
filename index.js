31644cbb2d5055eff49c10c9759d15134ed8f2b7
ghofrandayyat <ghofrandayyat@gmail.com>
2021-06-15 18:34:03 +0300

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 require('dotenv').config();
-const MONGODB_URL=process.env.MONGODB_URL;
+const MONGODB_URL=process.env.MONGODB_URL||'mongodb+srv://admin:aaaa@api0server.afeds.mongodb.net/test';
 const PORT = process.env.PORT||4444;
 const server = require('./src/server.js');
 server.start(PORT,MONGODB_URL);
